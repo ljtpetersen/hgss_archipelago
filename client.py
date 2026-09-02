8866affe8aa8000bbe94b9b8b2d7a64151999bc0
James Petersen <m@jamespetersen.ca>
2026-09-02 02:11:05 -0400

    Version 0.0.6

--- client.py
+++ client.py
@@ -141,6 +141,7 @@ AP_VERSION_DATA: Mapping[int, VersionData] = {
     version_int("0.0.3"): prev_version_data,
     version_int("0.0.4"): prev_version_data,
     version_int("0.0.5"): prev_version_data,
+    version_int("0.0.6"): prev_version_data,
 }
 
 @dataclass(frozen=True)
